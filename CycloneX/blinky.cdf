7eb16ac56c75104bf56108c70d1fa95bd2cb3f57
Dennis <denniswong178@gmail.com>
2024-02-20 21:51:50 +0800

    Cyclone X: blinky

---
+++ CycloneX/blinky.cdf
@@ -0,0 +1,15 @@
+/* Quartus Prime Version 23.1std.0 Build 991 11/28/2023 SC Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(10CL025YU256) Path("C:/Users/denni/Documents/Code/CycloneX/") File("blinky.sof") MfrSpec(OpMask(1));
+	P ActionCode(Ign)
+		Device PartName(VTAP10) MfrSpec(OpMask(0));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
--- CycloneX/blinky/blinky.v
+++ CycloneX/blinky/blinky.v
@@ -1,34 +1,19 @@
+module blinky(input clk, output [1:0]led);
 
-module blinky(input clock, input clk, output LED, output LED1);
-
-reg [25:0]count =0;
-reg [25:0]count_1 =0;
-reg status;
-reg status_1;
-
-always @ (posedge clock)
-begin
-	count <= count + 1;
-	
-	//if (count == 24_999_999)
-	if (count == 49_999_999)
-	begin
-		count <= 0;
-		status <= ~status;
-	end
-	
-end 
+reg [26:0] count;
+reg led_output;
 
 always @ (posedge clk)
 begin
-	count_1 <= count_1 + 1;
-	if (count_1 == 24_999_999)
+	if(count==49_999_999)
 	begin
-		count_1 <= 0;
-		status_1 <= ~status_1;
+		count <= 0;
+		led_output <= ~ led_output;
 	end
-end
+	else
+		count <= count + 1;
+end 
+assign led[0] = led_output;
+assign led[1] = led_output;
 
-assign LED = status;
-assign LED1 = status_1;
-endmodule 
+endmodule
