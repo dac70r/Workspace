2fda47d192a6c3b7630fdbde07d9a977ffb0613a
dac70r <denniswong178@gmail.com>
2025-03-14 17:48:05 +0800

    SPI_Write_Working

--- Cyclone_10_LP/NIOS_II/My_First_NIOS_II.v
+++ Cyclone_10_LP/NIOS_II/My_First_NIOS_II.v
@@ -1,25 +1,32 @@
 
 module My_First_NIOS_II(
 
-	input 			CLOCK_50,
-	output [7:0] 	LEDG,
-	output [7:0] 	RESET_ADC,
-	input 			SPI_MISO,
-	output 			SPI_MOSI,
-	output 			SPI_CLOCK,
-	output 			SPI_SS
+	input 			CLOCK_50,						// System Clock
+	output [7:0] 	LEDG,								// remove in the future
+	
+	output [7:0] 	RESET_ADC,						// remove in the future
+	
+	input 			RST_N,
+	input 			SPI_WR_TRIG, 					
+	input 			ADC1_MISO,						//	ADC1 - MISO
+	output			ADC1_MOSI,						// ADC1 - MOSI
+	output			ADC1_CLOCK,						//	ADC1 - SCLK
+	output			ADC1_SS,							//	ADC1 - Chip Select
+	output 			ADC1_RESET,						// ADC1 - Reset
+	
+	input 			SPI_MISO,						// ADC0 - MISO
+	output 			SPI_MOSI,						// ADC0 - MOSI
+	output 			SPI_CLOCK,						// ADC0 - SCLK
+	output 			SPI_SS,							// ADC0 - Chip Select
+	output 			ADC0_RESET,						// ADC0 - Reset
+	
+	input 			esc_spi_external_MISO,     // ESC_SPI - MISO      
+	output        	esc_spi_external_MOSI,     // ESC_SPI - MISO           
+	output        	esc_spi_external_SCLK,     // ESC_SPI - MISO           
+	output        	esc_spi_external_SS_n      // ESC_SPI - MISO          	
 );
 
-// Placeholder signals for now
-reg 			esc_spi_external_MISO;           //         esc_spi_external.MISO
-reg        	esc_spi_external_MOSI;           //                         .MOSI
-reg        	esc_spi_external_SCLK;           //                         .SCLK
-reg        	esc_spi_external_SS_n;           //                         .SS_n
-reg         adc_1_spi_external_MISO;         //       adc_1_spi_external.MISO
-reg        	adc_1_spi_external_MOSI;         //                         .MOSI
-reg       	adc_1_spi_external_SCLK;         //                         .SCLK
-reg        	adc_1_spi_external_SS_n;         //                         .SS_n
-		
+		/*
 My_First_NIOS_II_Platform_Designer u0 (
 		.clk_clk (CLOCK_50),   
 		.gpio_external_connection_export(LEDG),			// led
@@ -28,14 +35,26 @@ My_First_NIOS_II_Platform_Designer u0 (
 		.spi_0_external_MOSI(SPI_MOSI),             		// spi_0_external.MOSI
 		.spi_0_external_SCLK(SPI_CLOCK),             	// spi_0_external.SCLK
 		.spi_0_external_SS_n(SPI_SS),              		// spi_0_external.SS
+		
 		.esc_spi_external_MISO(esc_spi_external_MISO),           //         esc_spi_external.MISO
 		.esc_spi_external_MOSI(esc_spi_external_MOSI),           //                         .MOSI
 		.esc_spi_external_SCLK(esc_spi_external_SCLK),           //                         .SCLK
 		.esc_spi_external_SS_n(esc_spi_external_SS_n),           //                         .SS_n
-		.adc_1_spi_external_MISO(adc_1_spi_external_MISO),         //       adc_1_spi_external.MISO
-		.adc_1_spi_external_MOSI(adc_1_spi_external_MOSI),         //                         .MOSI
-		.adc_1_spi_external_SCLK(adc_1_spi_external_SCLK),         //                         .SCLK
-		.adc_1_spi_external_SS_n(adc_1_spi_external_SS_n)         //                         .SS_n
-	);
+		
+		.adc_1_spi_external_MISO(ADC1_MISO),         
+		.adc_1_spi_external_MOSI(ADC1_MOSI),         
+		.adc_1_spi_external_SCLK(ADC1_CLOCK),         
+		.adc_1_spi_external_SS_n(ADC1_SS)         
+	); */
 
+// Instatiation of SPI Interface
+adc_interface adc_uut1 (.system_clock(CLOCK_50),
+								.rst_n(RST_N),
+								.ADC_SPI_MISO(ADC1_MISO),
+								.ADC_SPI_MOSI(ADC1_MOSI),
+								.ADC_SPI_SCLK(ADC1_CLOCK),
+								.ADC_SPI_CS(ADC1_SS),
+								.ADC_RESET(ADC1_RESET),
+								.SPI_WR_TRIG(SPI_WR_TRIG)
+								);
 endmodule
--- Cyclone_10_LP/NIOS_II/db/ip/sld4f5a1263/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ Cyclone_10_LP/NIOS_II/db/ip/sld4f5a1263/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 8e9cddcf513354e278a3
+//   DESIGN_HASH   : fd4aaa6a95f30e078556
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h8e9cddcf513354e278a3 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hfd4aaa6a95f30e078556 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
